0ddd6e819669fa6efb4ceddb88ba48f706c18449
Night Cat <107802416+MHNightCat@users.noreply.github.com>
2023-12-08 12:14:55 +0800

    update typpe and lauguage name

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,12 +1,19 @@
-import {notFound} from 'next/navigation';
- 
+import { notFound } from "next/navigation";
+import React from "react";
+
 // Can be imported from a shared config
-const locales = ['en', 'zh-TW'];
- 
-export default function LocaleLayout({children, params: {locale}}) {
+const locales = ["en", "zh-tw"];
+
+export default function LocaleLayout({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode;
+  params: { locale: "en" | "zh-tw" };
+}) {
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
- 
+
   return (
     <html lang={locale}>
       <body>{children}</body>
